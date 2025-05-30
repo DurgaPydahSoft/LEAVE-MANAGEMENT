2765a64d87518e1c845c467e8cb4884d0ab9e2ac
DurgaPydahSoft <durgaprasad@pydahsoft.in>
2025-05-30 21:07:05 +0530

    cors more updated

--- pydah-lms-frontend/src/services/api.js
+++ pydah-lms-frontend/src/services/api.js
@@ -1,18 +1,21 @@
 import axios from 'axios';
+import config from '../config';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: config.API_BASE_URL,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  withCredentials: true
 });
 
-// Add a request interceptor
+// Add request interceptor to add auth token
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers['x-auth-token'] = token;
     }
     return config;
   },
@@ -29,15 +32,31 @@ api.interceptors.response.use(
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx
       console.error('Response Error:', error.response.data);
+      
+      // Handle CORS errors specifically
+      if (error.response.status === 0) {
+        console.error('CORS Error: Unable to reach the server');
+        return Promise.reject({ 
+          msg: 'Unable to reach the server. Please check your connection.',
+          isCorsError: true
+        });
+      }
+      
       return Promise.reject(error.response.data);
     } else if (error.request) {
       // The request was made but no response was received
       console.error('Request Error:', error.request);
-      return Promise.reject({ msg: 'No response from server' });
+      return Promise.reject({ 
+        msg: 'No response from server. Please try again.',
+        isNetworkError: true
+      });
     } else {
       // Something happened in setting up the request that triggered an Error
       console.error('Error:', error.message);
-      return Promise.reject({ msg: error.message });
+      return Promise.reject({ 
+        msg: error.message,
+        isError: true
+      });
     }
   }
 );
--- pydah-lms-frontend/src/utils/authAxios.js
+++ pydah-lms-frontend/src/utils/authAxios.js
@@ -1,13 +1,50 @@
 import axios from 'axios';
+import config from '../config';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const authAxios = axios.create({
+  baseURL: config.API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  withCredentials: true
+});
 
-export const createAuthAxios = (token) => {
-  return axios.create({
-    baseURL: API_BASE_URL,
-    headers: {
-      'Authorization': `Bearer ${token}`,
-      'Content-Type': 'application/json'
+// Add request interceptor
+authAxios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['x-auth-token'] = token;
     }
-  });
-}; 
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Add response interceptor
+authAxios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      console.error('Auth Response Error:', error.response.data);
+      return Promise.reject(error.response.data);
+    } else if (error.request) {
+      console.error('Auth Request Error:', error.request);
+      return Promise.reject({ 
+        msg: 'No response from server. Please try again.',
+        isNetworkError: true
+      });
+    } else {
+      console.error('Auth Error:', error.message);
+      return Promise.reject({ 
+        msg: error.message,
+        isError: true
+      });
+    }
+  }
+);
+
+export default authAxios; 
